19371f67eba49b2cebacc7840d5ab184edde7ac0
Brendan C <bacurrie0@gmail.com>
2022-02-03 01:33:28 -0500

    Added Tab  functionality

--- GUITest.py
+++ GUITest.py
@@ -10,10 +10,22 @@ entry = rf.getLatest(rssFeed)
 # loading a theme (possible make option later?)
 sg.theme('DarkPurple7')
 # All the stuff inside your window.
-layout = [[sg.Text(entry.title)],
-          [sg.Text(text=entry.link, enable_events=True)],
-          [sg.Button('HERE I WANT TO ADD RSS FEEDS.'), sg.Button('Cancel')]]
+# tab1_layout = [[sg.Text(entry.title
+#                         [sg.Text(text=entry.link, enable_events=True)],
+#                         [sg.Button('HERE I WANT TO ADD RSS FEEDS.'), sg.Button('Cancel')]
+#
+# tab2_layout = [[sg.Text("test")],
+#           [sg.Text(text=entry.link, enable_events=True)],
+#           [sg.Button('HERE I WANT TO ADD RSS FEEDS.'), sg.Button('Cancel')]]
+tab1_layout = [[sg.Text(entry.title)],
+               [sg.Text(text=entry.link, enable_events=True)],
+               [sg.Button('HERE I WANT TO ADD RSS FEEDS.')]]
 
+tab2_layout = [[sg.T('This is inside tab 2')],
+               [sg.In(key='in')]]
+
+layout = [[sg.TabGroup([[sg.Tab('Tab 1', tab1_layout, tooltip='tip'),
+                        sg.Tab('Tab 2', tab2_layout)]], tooltip='TIP2')]]
 # Create the Window
 window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
---
+++ Read_Me.txt
@@ -0,0 +1,17 @@
+RssFeeder:
+
+is a basic application to store rssfeeds and display them so that they are easily accessible. Designed using PySimpleGUI
+hopefully by doing this, it will be an unobtrusive friendly app that can easily be put on any computer.
+
+Things to do:
+
+Create a way to collect which rss feeds the user wants to see.
+Take most recently post article and post them in most recent order.
+
+
+Possible changes:
+Themes
+Database to store all of the RSS feeds.
+
+
+something to remove post that have been clicked on.
